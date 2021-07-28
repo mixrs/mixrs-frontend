8ecf97f6dd5e78060f1d8c84b4cb6ee8b107fd2d
Tak1za <varungupta2015135@gmail.com>
2021-07-28 11:01:33 +0530

    Added react-router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,35 @@
-import React from "react";
-import Navbar from "../navbar/Navbar";
+import React, {useState} from "react";
 import "./App.css";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { Switch, Route } from "react-router-dom";
+import Top from "../top/Top";
+import Channels from "../channels/Channel";
+import Sidebar from "../sidebar/Sidebar";
+import { Layout } from "antd";
+import Dashboard from "../dashboard/Dashboard";
+
+const { Content } = Layout;
 
 library.add(fab);
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="App">
-      <Navbar />
+      <Layout style={{ height: "100%" }}>
+        <Sidebar collapsed={collapsed} />
+        <Layout className="MainContainer">
+          <Top collapsed={collapsed} toggleCollapse={setCollapsed} />
+          <Content className="ContentContainer">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/channels" component={Channels} />
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
