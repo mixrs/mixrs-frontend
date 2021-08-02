a0839ae9e83f1568c80f3518118ec243d91c8a92
Tak1za <varungupta2015135@gmail.com>
2021-08-02 21:12:14 +0530

    removed top bar and made sidebar to remain collapsed

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,23 +2,23 @@ import React from "react";
 import "./Sidebar.css";
 import { Layout, Menu } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLayerGroup, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faUsers, faHome } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const { Sider } = Layout;
 
-function Sidebar({ collapsed }) {
+function Sidebar() {
   return (
-    <Sider trigger={null} collapsible collapsed={collapsed}>
+    <Sider trigger={null} collapsed={true}>
       <div className="Logo">
         <FontAwesomeIcon icon={["fab", "mendeley"]} />
         <p>MiXrS</p>
       </div>
-      <Menu theme="dark" mode="inline">
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["0"]}>
         <Menu.Item key="0" icon={<FontAwesomeIcon icon={faHome} />}>
           <Link to="/">Dashboard</Link>
         </Menu.Item>
-        <Menu.Item key="1" icon={<FontAwesomeIcon icon={faLayerGroup} />}>
+        <Menu.Item key="1" icon={<FontAwesomeIcon icon={faUsers} />}>
           <Link to="/channels">Channels</Link>
         </Menu.Item>
       </Menu>
