ed6bb99402258f741b12d67ae3e3b331d37f1492
Tak1za <varungupta2015135@gmail.com>
2021-08-06 02:59:59 +0530

    Added code to create a new post

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,26 +1,56 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 import { useParams } from "react-router-dom";
 import { getAllPosts } from "../services/Posts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Post from "../Post/Post";
+import "./PostList.css";
+import NewPost from "../NewPost/NewPost";
 
 function PostList() {
   let { channelId } = useParams();
 
+  const [alert, setAlert] = useState(false);
   const [posts, setPosts] = useState([]);
+  const [showPostForm, setShowPostForm] = useState(false);
 
   useEffect(() => {
     let mounted = true;
+
+    if (posts.length && !alert) {
+      return;
+    }
+
     getAllPosts(channelId).then((items) => {
       if (mounted) {
         setPosts(items.data);
+        setAlert(false);
       }
     });
 
     return () => (mounted = false);
-  }, [channelId]);
+  }, [posts, alert, channelId]);
+
+  const showDrawer = () => {
+    setShowPostForm(true);
+  };
+
+  const onClose = () => {
+    setShowPostForm(false);
+  };
 
   return (
-    <div>
+    <div className="PostList">
+      <Button
+        type="primary"
+        shape="round"
+        icon={<FontAwesomeIcon icon={faPlus} />}
+        onClick={showDrawer}
+      >
+        New Post
+      </Button>
+      <NewPost onClose={onClose} visible={showPostForm} setAlert={setAlert} />
       {posts.map((post) => {
         return <Post data={post} key={post.id} />;
       })}
